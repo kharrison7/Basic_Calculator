2ee24b6a981f51b793cef89311bf510f74e9a009
Kevin Harrison <kharrison7@elon.edu>
2017-08-08 17:52:35 -0400

    This overhauls a lot of code to make the calculator work using eval.

--- main.js
+++ main.js
@@ -1,139 +1,37 @@
-  console.log("JS Online");
-  console.log("JS Online");
+console.log("JS On");
 
-let blocks_Clicked = 0;
 
-// This amkes and runs the answer box.
-function accessTile(){
-console.log("block clicked");
-
-blocks_Clicked++;
-console.log("blocks clicked: " + blocks_Clicked);
-let liText = blocks_Clicked;
-// This moves the text to the H2.
-    liH2.innerHTML = liText ;
-// This adds the H2 to the li tile.
-    answer_Box.appendChild( liH2 );
-// This adds the li to the itemList.
-    clockSpot.appendChild( answer_Box );
-}
-let clockSpot = document.querySelector( "#top_board" );
-let answer_Box       = document.createElement( "li" );
-answer_Box.setAttribute("class", "answer_Space");
-let liH2     = document.createElement( "h2" );
+let in_Answer_String = " ";
+let continuing_a_calculation = false;
+let answer_Field = document.getElementById("answer_goes_here");
 
 
 
-
-
-
-// This places the clear box.
-let attach = document.querySelector( "#top_board" );
-let form       = document.createElement( "li" );
-// This creates an H2 element.
-let form_H2     = document.createElement( "h2" );
-// This places the random_Set text into the liText.
-let form_Text = document.createTextNode( "Clear" );
-// This makes the tile respond to being clicked.
-form.addEventListener('click', accessTile);
-// This creates a class for the tile and set it to hide.
-form.setAttribute("class", "clear");
-form.addEventListener('click', accessTile);
-// form.setAttribute("class", "answer_Space");
-// This moves the text to the H2.
-  form_H2.appendChild( form_Text );
-// This adds the H2 to the li tile.
-  form.appendChild( form_H2 );
-// This adds the li to the itemList.
-  attach.appendChild( form );
-
-
-let hold       = document.createElement( "div" );
-hold.setAttribute("id", "hold_This");
-
-  accessTile();
-
-
-
-
-
-
-// This places the rest of the buttons.
-function build_Calculator1(){
-  console.log("Calculator being built 1.");
-// This makes the itemList node attach to the total_board element on the HTML page.
-let itemList = document.querySelector( "#total_board" );
-// This makes a loop to go through the memor_Options array.
-  for( let i = 0; i < 16; i++ ){
-
-// This creates an li value to act as a tile.
-    let li       = document.createElement( "li" );
-
-// This creates an H2 element.
-    let liH2     = document.createElement( "h2" );
-
-// This places the random_Set text into the liText.
-    let liText = document.createTextNode( i );
-
-// This makes the tile respond to being clicked.
-  li.addEventListener('click', accessTile);
-
-// This creates a class for the tile and set it to hide.
-   li.setAttribute("class", "hide");
-
-  //  if( i === 1){
-  //  li.setAttribute("class", "answer_Space");
-  //  }
-
-// This sets each tile with an id based on position on the game board AND the card value.
-   li.setAttribute("id", "card" + i);
-// This moves the text to the H2.
-    liH2.appendChild( liText );
-// This adds the H2 to the li tile.
-    li.appendChild( liH2 );
-// This adds the li to the itemList.
-    itemList.appendChild( li );
+function input_Character(button_character, is_Arithmetic_Operation){
+  console.log("button is: " +button_character);
+  
+  if (continuing_a_calculation === true && is_Arithmetic_Operation === false){
+    in_Answer_String = " ";
+    answer_Field.textContent = " ";
   }
+  in_Answer_String += ""+button_character+"";
+  answer_Field.textContent += button_character;
+  console.log(in_Answer_String);
+  continuing_a_calculation = false;
 }
 
 
+// This takes the result string and computes an answer.
+function do_Math(){
+  let result = eval(in_Answer_String);
+  answer_Field.textContent = result;
+  continuing_a_calculation = true;
+  console.log("Answer Equals: " + result);
+}
 
-// function build_Calculator2(){
-//   console.log("Calculator being built 2.");
-// // This makes the itemList node attach to the total_board element on the HTML page.
-// let itemList = document.querySelector( "#total_board" );
-// // This makes a loop to go through the memor_Options array.
-//   for( let i = 0; i < 16; i++ ){
-//
-// // This creates an li value to act as a tile.
-//     let li       = document.createElement( "li" );
-//
-// // This creates an H2 element.
-//     let liH2     = document.createElement( "h2" );
-//
-// // This places the random_Set text into the liText.
-//     let liText = document.createTextNode( i );
-//
-// // This makes the tile respond to being clicked.
-//   li.addEventListener('click', accessTile);
-//
-// // This creates a class for the tile and set it to hide.
-//    li.setAttribute("class", "hide");
-//
-//   //  if( i === 1){
-//   //  li.setAttribute("class", "answer_Space");
-//   //  }
-//
-// // This sets each tile with an id based on position on the game board AND the card value.
-//    li.setAttribute("id", "card" + i);
-// // This moves the text to the H2.
-//     liH2.appendChild( liText );
-// // This adds the H2 to the li tile.
-//     li.appendChild( liH2 );
-// // This adds the li to the itemList.
-//     itemList.appendChild( li );
-//   }
-// }
-
-build_Calculator1();
-// build_Calculator2();
+// This clears the answer_Field.
+function clear_Button(){
+  in_Answer_String = " ";
+  answer_Field.textContent = " ";
+  console.log("Answer Cleared!")
+}
