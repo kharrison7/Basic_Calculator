1be96ae8ae919f0b243ab59415761e8fbc6c49ca
Kevin Harrison <kharrison7@elon.edu>
2017-08-08 21:15:25 -0400

    THis cleans up some code.

--- main.js
+++ main.js
@@ -6,26 +6,18 @@ let continuing_a_calculation = false;
 let answer_Field = document.getElementById("answer_goes_here");
 
 
-
 function input_Character(button_character, is_Arithmetic_Operation){
   console.log("button is: " +button_character);
-  
-  if (continuing_a_calculation === true && is_Arithmetic_Operation === false){
-    in_Answer_String = " ";
-    answer_Field.textContent = " ";
-  }
+  // This adds the character of the button to the answer field.
   in_Answer_String += ""+button_character+"";
   answer_Field.textContent += button_character;
   console.log(in_Answer_String);
-  continuing_a_calculation = false;
 }
-
 
 // This takes the result string and computes an answer.
 function do_Math(){
   let result = eval(in_Answer_String);
   answer_Field.textContent = result;
-  continuing_a_calculation = true;
   console.log("Answer Equals: " + result);
 }
 
